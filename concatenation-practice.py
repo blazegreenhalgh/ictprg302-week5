2e3bae4b7996b20ae5fe3c53cf1074ef9f3bc629
Blaze Greenhalgh <blazegreenhalgh1943@gmail.com>
2025-03-11 06:35:03 +0800

    Completed concatenation exercise

--- concatenation-practice.py
+++ concatenation-practice.py
@@ -1,32 +1,35 @@
+
+
+
 tic_tac = ("- X O\n" +
            "X - X\n" +
            "O - O\n")
 
 # what is the length of the string?
 # insert code here
+print(len(tic_tac))
 
 # make the following print True by substituting
 # each `?` with a **different** value
 # NOTE: **all** you need to do is substitute the `?` with integers!
 print(tic_tac[0] ==
-      tic_tac[?] ==
-tic_tac[?])  # True
+      tic_tac[8] ==
+tic_tac[14])  # True
 
 print(tic_tac[2] ==
-      tic_tac[?] ==
-tic_tac[?])  # True
+      tic_tac[6] == tic_tac[10])  # True
 
 print(tic_tac[5] ==
-      tic_tac[?] ==
-tic_tac[?])  # True
+      tic_tac[11] ==
+      tic_tac[17])  # True
 
 # combine two slices of the original string
 # for X to win the game:
-print(tic_tac[?:?] + 'X' + tic_tac[?:?])
-
-# combine two slices of the original string
-# for O to win the game:
-print(tic_tac[?:?] + 'O' + tic_tac[?:?])
-
-# Bonus question, complete the following to print 'XXX': print(tic_tac[?:?:?])
+print(tic_tac[6:7] + 'X' + tic_tac[10:11])
 #
+# # combine two slices of the original string
+# for O to win the game:
+print(tic_tac[12:13] + 'O' + tic_tac[16:17])
+
+# Bonus question, complete the following to print 'XXX':
+print(tic_tac[2:11:4])
